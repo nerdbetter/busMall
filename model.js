6ea995a06d2511747ed0c43de4432d02443351c0
nerdbetter <jasontlogan@gmail.com>
2017-06-19 17:05:31 -0500

    started js for images

--- model.js
+++ model.js
@@ -1,8 +1,12 @@
 ('use strict');
 
-function productChoice(name, file, numberShown, votes){
-  this.name = name;
+function productChoice(name, file){
+  this.name = name,
   this.file = file;
-  this.numberShown = [];
-  this.votes = [];
 };
+
+var boots = new productChoice(boots, 'images/boots.jpg');
+var chair = new productChoice(chair, 'images/chair.jpg');
+var scissors = new productChoice(scissors, 'images/scissors.jpg');
+var waterCan = new productChoice(waterCan, 'images/water_can.jpg');
+var wineGlass = new productChoice(wineGlass, 'images/wine_glass.jpg');
