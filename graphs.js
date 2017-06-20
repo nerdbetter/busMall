c3351e5b4eaee2e69889e4bf9057235730134220
nerdbetter <jasontlogan@gmail.com>
2017-06-20 15:35:19 -0500

    graph is showing

--- graphs.js
+++ graphs.js
@@ -1,1 +1,13 @@
 ('use strict');
+
+var productVotes = new Chart(document.getElementById('productVotes').getContext('2d'), {
+  type: 'bar',
+  data:{
+    labels:['boots', 'chair', 'scissors', 'waterCan', 'wineGlass'],
+    datasets: [{
+      backgroundColor: ['#8e5ea2', '#8e5ea2','#8e5ea2','#8e5ea2','#8e5ea2'],
+      label: 'Votes',
+      data:[5, 7, 4, 7, 8],
+    }]
+  }
+});
