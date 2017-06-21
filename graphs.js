b961ae78992e4b7b30ced5d08029059dd3ec7b75
nerdbetter <jasontlogan@gmail.com>
2017-06-20 21:05:48 -0500

    style is done. Graph is built. Need to get data to move to Graph.

--- graphs.js
+++ graphs.js
@@ -1,13 +1,18 @@
 ('use strict');
-
+Chart.defaults.global.defaultFontFamily = 'Palanquin Dark';
+Chart.defaults.global.defaultFontSize = 20;
 var productVotes = new Chart(document.getElementById('productVotes').getContext('2d'), {
-  type: 'bar',
+  type: 'horizontalBar',
   data:{
     labels:['boots', 'chair', 'scissors', 'waterCan', 'wineGlass'],
     datasets: [{
-      backgroundColor: ['#8e5ea2', '#8e5ea2','#8e5ea2','#8e5ea2','#8e5ea2'],
+      backgroundColor: '#E2231A',
       label: 'Votes',
       data:[5, 7, 4, 7, 8],
-    }]
+    }],
+    options:{
+      responsive: true,
+      maintainAspectRatio: false,
+    }
   }
 });
