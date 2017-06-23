f4c9ea0783f8af6ab23c501e56adcf1ec9fefbbb
nerdbetter <jasontlogan@gmail.com>
2017-06-23 00:23:51 -0500

    forgot to save before last commit so changes are not there

--- storage.js
+++ storage.js
@@ -5,3 +5,11 @@ function makeThings(index){
   var votes = productArray[index].vote;
   return {shown: shown, vote: vote};
 }
+function stringThings(){
+  var stringy = {};
+  for(var i = 0; i < productArray.length; i++){
+    stringy[productArray[i].itemName] = stringThings(i);
+  }
+  return JSON.stringify(obj);
+}
+window.localStorage.storedObjects = stringThings();
