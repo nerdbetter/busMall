afcf212900576e881ee13b3905341e763f965b4f
nerdbetter <jasontlogan@gmail.com>
2017-06-23 20:52:36 -0500

    style updates

--- storage.js
+++ storage.js
@@ -1,10 +1,10 @@
 ('use strict');
 
-function makeThings(index){
+/*function makeThings(index){
   var shown = productArray[index].shown;
   var votes = productArray[index].vote;
   return {shown: shown, vote: vote};
-}
+}*/
 function stringThings(){
   var stringy = {};
   for(var i = 0; i < productArray.length; i++){
